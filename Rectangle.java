8cce7cc3d17c85da466d4025053da92019a61d8d
serega28465 <serega28465@gmail.com>
2016-11-20 21:20:01 +0200

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -4,19 +4,37 @@ package core.java1;
 
 public class Rectangle {
 	
+	public int count;
 	
-	int width;
-	int length;
+	public int width;
+	public int length;
 
+	//конструктор з параметрами
 	public Rectangle(int width, int length) {
 		this.width = width;
 		this.length = length;
+		count++;
 		System.out.println("Прямокутник створено");
 	}
-
+	
+	//конструктор без параметрів
+	public Rectangle(){
+		count++;
+	}
+	
+	static int rectS(int width, int length){
+		return width * length;
+	}
+	
+	static int rectP(int width, int length){
+		return width+length+width+length;
+	}
+	
+	
+	
 	public void info() {
-		System.out.println("Площа прямокутника " + width * length + " cm");
-		System.out.println("Периметр прямокутника " + (width+length+width+length) + " cm");
+		System.out.println("Площа прямокутника = " + rectS(width,length) + " cm");
+		System.out.println("Периметр прямокутника = " + rectP(width,length) + " cm");
 	}
 
 }
