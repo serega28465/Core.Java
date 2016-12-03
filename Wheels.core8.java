a856ca7acb6d3f50b54db1a04f402c31265488c0
serega28465 <serega28465@gmail.com>
2016-12-03 16:43:03 +0200

    Update Wheels.core8.java

--- Wheels.core8.java
+++ Wheels.core8.java
@@ -1,15 +1,18 @@
-package core.java7;
+package core.java8;
 
 public class Wheels {
 	
 	private double w;
 	
+	private String z;
 	
-	
-	public Wheels(double w) {
-		this.w = w;
-	}
 
+
+	public Wheels(double w, String z) {
+		super();
+		this.w = w;
+		this.z = z;
+	}
 
 
 	public Wheels(){}
@@ -29,12 +32,25 @@ public class Wheels {
 		this.w = w;
 	}
 
+	
+
+	public String getZ() {
+		return z;
+	}
+
+
+	public void setZ(String z) {
+		this.z = z;
+	}
 
 
 	@Override
 	public String toString() {
-		return "Колеса [ діаметр - " + w + " sm ]";
+		return "Колеса [ діаметр - " + w + " sm , Резина = " + z + " ]";
 	}
+
+
+	
 	
 	
 	
