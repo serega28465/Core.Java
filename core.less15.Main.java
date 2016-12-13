47b3a05c81e4d5a66c60f4aff0f6867da3760393
serega28465 <serega28465@gmail.com>
2016-12-13 18:32:58 +0200

    Update core.less15.Main.java

--- core.less15.Main.java
+++ core.less15.Main.java
@@ -1,5 +1,13 @@
 package core.lesson15;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import core.lesson4.Circle;
+import core.lesson4.Geometric;
+import core.lesson4.Rectangle;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -11,7 +19,15 @@ public class Main {
 		for (Object object : list) {
 			System.out.println(object);
 		}
-
+		List<Geometric> list2 = new ArrayList<>(Arrays.asList(new Rectangle(4, 5), new Circle(5)));
+		print(Arrays.asList(new Rectangle(4, 5), new Circle(5)));
 	}
-
+	
+	static void print(Iterable<? extends Geometric> list){
+		for (Geometric geometric : list) {
+			geometric.print();
+		}
+	}
+	
+	
 }
