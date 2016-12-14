f958baba5d50c113e3e199a381c2b0b552a0316c
serega28465 <serega28465@gmail.com>
2016-12-14 13:34:38 +0200

    Create Main.nasted.java

---
+++ Main.nasted.java
@@ -0,0 +1,23 @@
+package core.nasted;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Collection list = new Collection();
+		list.add(1);//1
+		list.add(5);//2
+		list.add(7);//3
+		list.add(2);//4
+		list.add(8);//5
+		list.add(7);//6
+		list.add(9);//7
+		list.add(2);//8
+		list.add(1);//9
+		list.add(3);//10
+		list.add(4);//11
+		list.add(5);//12
+		System.out.println(list);
+		
+	}
+
+}
