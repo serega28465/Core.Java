7c7f7e25524ca7af64a4c3cf44edb38af060b8b2
serega28465 <serega28465@gmail.com>
2016-12-15 17:37:00 +0200

    Create Single.potok.java

---
+++ Single.potok.java
@@ -0,0 +1,24 @@
+package core.lesson;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Singleton {
+	
+	private final Deque<Integer> que = new LinkedList<>();
+	
+	private static final Singleton instance = new Singleton();
+	
+	public static Singleton getInstance(){
+		return instance;
+	}
+	
+	public void addTask(int task){
+		que.addLast(task);
+	}
+	
+	public Integer getTask(){
+		if(que==null)return null;
+		return que.poll();
+	}
+}
