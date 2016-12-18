c685768db1511e2cd76362a434f0c6d715c426d2
serega28465 <serega28465@gmail.com>
2016-12-18 18:21:53 +0200

    Create Main.nast.java

---
+++ Main.nast.java
@@ -0,0 +1,46 @@
+package core.nasted;
+
+import core.nasted.Collection.Nasted;
+import core.nasted.Collection.ToLast;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Collection collection = new Collection();
+		collection.add();
+		Collection.ToLast tola = collection.new ToLast();
+		while(tola.hasNext()){
+			
+		collection.equals(tola.next());
+		}
+		//from first to last
+		System.out.println(collection);
+		System.out.println();
+		
+		//Collection collection2 = new Collection();
+		collection.add();
+		Collection.ToFirst tofi = collection.new ToFirst();
+		int count = 0;
+		while(tofi.hasNext()){
+			
+			count++;
+			collection.equals(tofi.next());	
+			
+		}
+		//from last to first
+		System.out.println(collection);
+		System.out.println();
+		
+		//local class
+		collection.local();
+		System.out.println(collection);
+		System.out.println();
+		
+		//Nasted class
+		Collection.Nasted nas = new Nasted();
+		nas.per(args);
+		System.out.println(nas);
+		
+		//Anonimous class
+	}
+}
