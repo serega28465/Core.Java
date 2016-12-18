4a0e2920398794ff857645fd30f1a8374232cd58
serega28465 <serega28465@gmail.com>
2016-12-18 14:46:07 +0200

    Update Main.thread.java

--- Main.thread.java
+++ Main.thread.java
@@ -1,6 +1,4 @@
-package core.dz1;
-
-
+package core.thread;
 
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
@@ -8,25 +6,22 @@ import java.util.concurrent.Executors;
 
 public class Main {
 
-	private static ExecutorService service = Executors.newFixedThreadPool(4);
-	
+	private static ExecutorService service = Executors.newFixedThreadPool(5);
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Яку кількість чисел Фібоначі ви хотіли б вивести на екран?");
 		int c = sc.nextInt();
-		new RunnableThread(100, c);
-		new MyThread(c);
 		
+		new MyThread(c);
+		new RunnableThread(100, c);
 		
 		
 		new Thread(()->{
 			
 		}).start();
 		service.shutdown();
-		
-		sc.close();
 	}
 
 }
